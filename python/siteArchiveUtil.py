1a0bff823d83ef40316eb59039471a7a052d3c53
cambot <jcmaci03@gmail.com>
2015-10-07 10:26:13 -0400

    Add archiveSites() to take in a file of sites, produce *.zip files, and generate htaccess rewrite rules.

--- python/siteArchiveUtil.py
+++ python/siteArchiveUtil.py
@@ -6,7 +6,36 @@ import csv
 
 '''
 Utility intended to aid in archiving snapshots of a list of websites.
+
+Provide a *.tsv file with the following columns:
+	- URL: the full url to grab a snapshot of
+	- Name: the name for the zip file
+	- Redirect: URL to redirect to
+
+archiveSites('sample.tsv')
+	generates:
+		*.zip files
+		RewriteRules.txt that you can copy into an .htaccess file
 '''
+
+def archiveSites(file_name):
+	rewriteRules = []
+	sites = loadTSVFile(file_name)
+	for website in sites:
+		print "archive " + website["URL"] + " as " + website["Name"] + " to " + website["Redirect"]
+		archive_site(website["URL"], website["Name"])
+		rewriteRules.append(generateRewriteRule(website["URL"], website["Redirect"]) )
+	with open('RewriteRules.txt', 'a+') as file_handle:
+		for line in rewriteRules:
+			file_handle.write(line + "\n")
+	file_handle.close()
+	return
+
+
+# Ex: RewriteRule ^erniepyle(.*)$ http://mediaschool.indiana.edu/erniepyle/$1 [R=301,NC,L]
+def generateRewriteRule(old_url, new_url):
+	subsite = extractSubsite(old_url)
+	return "RewriteRule ^" + subsite + "(.*)$ " + new_url + "$1 [R=301,NC,L]"
 
 # Load a *.tsv file and return a list of dictionaries of the values
 #    tsv = Tab separated values
@@ -15,17 +44,21 @@ def loadTSVFile(file_name):
 		return [row for row in csv.DictReader(file_handle, delimiter="\t")]
 
 
-def archive_site(target_url):
+def archive_site(target_url, zipName=None):
+	x = target_url.split("//")[1]   # journalism.indiana.edu/resources/webediting
+	oldDirName = x.split('/')[0]    # journalism.indiana.edu
+	if zipName is None:
+		zipName = x.replace('/', '_')   # journalism.indiana.edu_resources_webediting
 	wget_wrapper(target_url)
-	x = target_url.split("//")[1]
-	oldDirName = x.split('/')[0]
-	newDirName = x.replace('/', '_')
-	print "Renaming " + oldDirName + " to " + newDirName
-	os.rename(oldDirName, newDirName)
-	zip_folder(newDirName, newDirName + '.zip')
-	print "Archived to " + newDirName + '.zip'
-	shutil.rmtree(newDirName)
+	print "Renaming " + oldDirName + " to " + zipName
+	os.rename(oldDirName, zipName)
+	zip_folder(zipName, zipName + '.zip')
+	print "Archived to " + zipName + '.zip'
+	shutil.rmtree(zipName)
 
+def extractSubsite(url):
+	x = url.rsplit("//")[1]
+	return "/".join(x.rsplit('/')[1:])
 
 # grabs the supplied website url from the web and copies it to the current directory
 # Dependency: wget is not natively available on Mac or Windows
