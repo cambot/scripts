3e846e100e3b0a1afebf3c32dde3b7838523d212
Cameron <jcmaci03@gmail.com>
2016-04-15 10:08:44 -0400

    Skip empty rows.

--- php/loadCSV.php
+++ php/loadCSV.php
@@ -2,13 +2,19 @@
 
 function loadCSVFile($fileName) {
 	if ( ($file = fopen($fileName, 'r')) !== FALSE ) {
+		$emptyRow = array(null);
+
 		$row = 1;
 		$values = array();
 		$fields = null;
+
 		while (($dataRow = fgetcsv($file, 4096)) !== FALSE ) {
 			if ( $fields === null ) {
 				$fields = $dataRow;
 				continue;
+			}
+			if ( $dataRow === $emptyRow ) {
+				continue;  // Skip empty rows
 			}
 			foreach ($dataRow as $key=>$value) {
 				$values[$row][$fields[$key]] = $value;
