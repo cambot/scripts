a649e50f28bb69197a0e61c0236f22af12e9b3d1
Cameron <jcmaci03@gmail.com>
2016-04-14 22:18:30 -0400

    Add php function to load csv and convert rows to associative arrays.

---
+++ php/loadCSV.php
@@ -0,0 +1,26 @@
+<?php
+
+function loadCSVFile($fileName) {
+	if ( ($file = fopen($fileName, 'r')) !== FALSE ) {
+		$row = 1;
+		$values = array();
+		$fields = null;
+		while (($dataRow = fgetcsv($file, 4096)) !== FALSE ) {
+			if ( $fields === null ) {
+				$fields = $dataRow;
+				continue;
+			}
+			foreach ($dataRow as $key=>$value) {
+				$values[$row][$fields[$key]] = $value;
+			}
+			$row++;
+		}
+		fclose($fileName);
+		return $values;
+	}
+	else {
+		echo "<p>Error. File not found: $fileName</p>";
+		return FALSE;
+	}
+}
+
