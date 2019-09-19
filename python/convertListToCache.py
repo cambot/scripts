70b947209ed01caea6b7976e83b75ffa731a4164
Cameron M <jcmaci03@gmail.com>
2019-09-19 10:21:13 -0400

    PEP8 Linting

--- python/convertListToCache.py
+++ python/convertListToCache.py
@@ -1,22 +1,22 @@
 #############
 # Convert List to cache set statements that can be pasted into a terminal
 #############
-def convertToCache(fname,var):
-	f=open(fname,'r')
-	file=f.readlines()
+def convertToCache(fname, var):
+	f = open(fname, 'r')
+	file = f.readlines()
 	f.close()
-	newFile=fname[0:fname.find(".")] + ".M"
-	f=open(newFile,'w')
+	newFile = fname[0:fname.find(".")] + ".M"
+	f = open(newFile, 'w')
 	if var.find('(') == -1:
-		var=var+'('
+		var = var + '('
 	for line in file:
 		if line.find("\n") != -1:
-			line=line[0:line.find("\n")]
+			line = line[0:line.find("\n")]
 		if (line.find("\t") != -1):
-			(index,value)=line.split('\t')
+			(index, value) = line.split('\t')
 			value = '"' + value + '"'
 		else:
-			index=line
+			index = line
 			value = '1'
 		val = 's ' + var + '"' + index + '")=' + value + '\n'
 		f.write(val)
