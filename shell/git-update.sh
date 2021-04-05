213887357afef1fc646d06ef6b1414cd39e29e62
Cameron M <jcmaci03@gmail.com>
2021-04-05 09:35:07 -0400

    Update git-update.sh
    add --prune

--- shell/git-update.sh
+++ shell/git-update.sh
@@ -2,8 +2,10 @@
 
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 git fetch origin $BRANCH
+git fetch origin --prune
 echo
 echo "  --[ Change Log ]--"
 git log --no-merges $BRANCH ..origin/$BRANCH --oneline
 echo
 git merge origin/$BRANCH
+exit 0
